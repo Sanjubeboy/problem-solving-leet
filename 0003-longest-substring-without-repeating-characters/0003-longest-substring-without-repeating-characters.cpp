4b8762e7c137d035834546a0785935811ccce19a
SANJAY KUMAR P <107700768+Sanjubeboy@users.noreply.github.com>
2023-03-06 19:18:03 +0530

    Time: 7 ms (91.54%), Space: 8.3 MB (63.61%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        // if(s.size() == 0)
+            // return -1;
+        
+        unordered_map<char,int> mp;
+        
+        int l = 0;
+        int ans = 0;
+        
+        for(int r=0; r<s.size(); r++)
+        {
+            
+            if(mp.find(s[r]) != mp.end()  and  (mp[s[r]]>=l and mp[s[r]] <=r))
+            {
+                
+                l = mp[s[r]] + 1;
+                ans = max(ans,r-l+1);
+                mp[s[r]] = r;
+                
+            }
+            
+            
+            else
+            {
+                
+                mp[s[r]] = r;
+                
+                ans = max(ans,r-l+1);
+                
+            }            
+        }
+        
+        return ans;
+        
+    }
+};
