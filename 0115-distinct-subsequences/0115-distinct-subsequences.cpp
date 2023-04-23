8df3275085f71d43fd4c9f3f6ae93ad4d6fbabea
SANJAY KUMAR P <107700768+Sanjubeboy@users.noreply.github.com>
2023-04-23 11:48:04 +0530

    Time: 23 ms (94.06%), Space: 6.5 MB (90.78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -48,22 +48,19 @@ public:
 //         return (int)dp[n][m];
         
         //space optimisation
-        vector<double> dp(m+1, 0), cur(m+1, 0);
+        vector<double> dp(m+1, 0);
         
         dp[0] = 1;
-        cur[0] = 1;
+       
         
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=m; j++)
+            for(int j=m; j>=1; j--)
             {
                 if(s[i-1] == t[j-1])
-                    cur[j] = dp[j-1] + dp[j];
-                
-                else
-                    cur[j] = dp[j];
+                    dp[j] = dp[j-1] + dp[j];
+            
             }
-            dp = cur;
         }
         
         return (int)dp[m];
