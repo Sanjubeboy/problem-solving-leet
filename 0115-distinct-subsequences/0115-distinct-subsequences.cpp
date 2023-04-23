77c20801a652867ca8b5b5bd8f05c8d4a4a750a3
SANJAY KUMAR P <107700768+Sanjubeboy@users.noreply.github.com>
2023-04-23 11:39:53 +0530

    Time: 53 ms (48.52%), Space: 18.5 MB (59.29%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,15 +4,15 @@ private:
     int helper(int i, int j, string &s, string &t, vector<vector<int>> &dp)
     {
         
-        if(j < 0)
+        if(j == 0)
             return 1;
-        if(i < 0)
+        if(i == 0)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(s[i] == t[j])
+        if(s[i-1] == t[j-1])
             return dp[i][j] =  helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
         
         return dp[i][j] = helper(i-1, j, s, t, dp);
@@ -24,9 +24,12 @@ public:
         int n = s.size();
         int m = t.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         
-        return helper(n-1, m-1, s, t, dp);
+        //recursion
+        return helper(n, m, s, t, dp);
+        
+        
         
     }
 };
