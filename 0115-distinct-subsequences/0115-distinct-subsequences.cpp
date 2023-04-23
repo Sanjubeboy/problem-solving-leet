2e4ef65ff2b140d91a4f69cf455db9897a0ca62f
SANJAY KUMAR P <107700768+Sanjubeboy@users.noreply.github.com>
2023-04-23 11:46:43 +0530

    Time: 30 ms (86.14%), Space: 6.5 MB (90.78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -24,28 +24,49 @@ public:
         int n = s.size();
         int m = t.size();
         
-        vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
+        // vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
         
         //recursion
         // return helper(n, m, s, t, dp);
         
         //tabulation
         
-        for(int i=0; i<=n; i++) dp[i][0] = 1;
+//         for(int i=0; i<=n; i++) dp[i][0] = 1;
+        
+//         for(int i=1; i<=n; i++)
+//         {
+//             for(int j=1; j<=m; j++)
+//             {
+//                 if(s[i-1] == t[j-1])
+//                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                
+//                 else
+//                     dp[i][j] = dp[i-1][j];
+//             }
+//         }
+        
+//         return (int)dp[n][m];
+        
+        //space optimisation
+        vector<double> dp(m+1, 0), cur(m+1, 0);
+        
+        dp[0] = 1;
+        cur[0] = 1;
         
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=m; j++)
             {
                 if(s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    cur[j] = dp[j-1] + dp[j];
                 
                 else
-                    dp[i][j] = dp[i-1][j];
+                    cur[j] = dp[j];
             }
+            dp = cur;
         }
         
-        return (int)dp[n][m];
+        return (int)dp[m];
         
         
         
