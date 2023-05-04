f999c0448d836d14578dc270cdbf089e573694e6
SANJAY KUMAR P <107700768+Sanjubeboy@users.noreply.github.com>
2023-05-04 22:38:05 +0530

    Time: 4 ms (82.94%), Space: 13.4 MB (17.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -6,68 +6,57 @@ public:
         int m = grid[0].size();
         int ans = 0;
         
-        queue<pair<int,pair<int,int>>> q;
-        
-        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int, pair<int,int>>> q;
+        vector<vector<int>> vis(n, vector<int> (m, 0));
         
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j] == 2)
-                {
-                    q.push({0,{i,j}});
+                if(grid[i][j] == 2){
+                    q.push({0, {i, j}});
                     vis[i][j] = 1;
                 }
+                    
             }
         }
         
-        vector<int> delrow = {0,1,-1,0};
-        vector<int> delcol = {1,0,0,-1};
+        vector<int> deli = {1,0,0,-1};
+        vector<int> delj = {0,1,-1,0};
         
         
         while(!q.empty())
         {
-            
             auto t = q.front();
-            q.pop();
-            
+            int i = t.second.first;
+            int j = t.second.second;
             int time = t.first;
             
-            int row = t.second.first;
-            int col = t.second.second;
+            ans = max(ans, time);
+            q.pop();
             
-            grid[row][col] = 2;
-            
-            ans = max(time,ans);
-            
-            for(int i=0; i<4; i++)
+            for(int k=0; k<4; k++)
             {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
+                int newi = i + deli[k];
+                int newj = j + delj[k];
                 
-                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m and grid[nrow][ncol] == 1 and !vis[nrow][ncol])
+                if(newi>=0 and newi<n and newj>=0 and newj<m and !vis[newi][newj] and grid[newi][newj] == 1)
                 {
-                    vis[nrow][ncol] = 1;
-                    q.push({time+1,{nrow,ncol}});
+                    q.push({time+1,{newi, newj}});
+                    vis[newi][newj] = 1;
                 }
-                
             }
-            
         }
         
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j] == 1)
-                {
+                if(vis[i][j] == 0 and grid[i][j] == 1)
                     return -1;
-                }
             }
         }
         
         return ans;
-        
     }
 };
